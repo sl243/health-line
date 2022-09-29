102c31aeee5cb0ca8f1639a72f90177252ecc71f
sl243 <slmiucse@gmail.com>
2022-09-29 19:48:44 +0600

    second question answer added

--- src/components/QuestionAns/QuestionAns.js
+++ src/components/QuestionAns/QuestionAns.js
@@ -15,7 +15,21 @@ const QuestionAns = () => {
                 <p className='answer'>State Management</p>
                 <p className='answer'>Programmatic Navigation</p>
             </div>
-            
+            <div>
+                <h2 className='question'>Difference between State and Props</h2>
+                <p className='answer'>1. Props are read-only. and 	State changes can be asynchronous.</p>
+                <p className='answer'>2. Props are immutable. and 	State is mutable.</p>
+                <p className='answer'>3. Props allow you to pass data from one component to other components as an argument. and State holds information about the components.</p>
+                <p className='answer'>4. Props can be accessed by the child component. and State cannot be accessed by child components.</p>
+                <p className='answer'>5. Props are used to communicate between components. and States can be used for rendering dynamic changes with the component.</p>
+                <p className='answer'>6. Stateless component can have Props. and Stateless components cannot have State.</p>
+                <p className='answer'>7. Props make components reusable. and State cannot make components reusable.</p>
+                <p className='answer'>8. Props are external and controlled by whatever renders the component. and The State is internal and controlled by the React Component itself.</p>
+            </div>
+            <div>
+                <h2 className='question'>What are work useeffect in react ?</h2>
+                <p className='answer'>JSX</p>
+            </div>
         </div>
     );
 };
