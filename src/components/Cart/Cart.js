d9e24486965bde86b5e9718efbe8df5d44373f70
sl243 <slmiucse@gmail.com>
2022-09-29 13:56:11 +0600

    cart section all items added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,43 @@
 import React from 'react';
+import logo from '../../images/logo.jpeg';
 import './Cart.css'
 
-const Cart = () => {
+const Cart = ({cart}) => {
+    
     return (
         <div>
-            <h1>Cart Profile</h1>
+            <h1>Cart Profile: {cart.length}</h1>
+                <div className='logo'>
+                    <img src={logo} alt=''></img>
+                    <div className='profile-info'>
+                        <h4>Md. Shamim Hossain</h4>
+                        <p>Paltan, Dhaka-1000</p>
+                    </div>
+                </div>
+                <div className='details'>
+                    <p>75kg Weight</p>
+                    <p>5.5 height</p>
+                    <p>28yrs Age</p>
+                </div>
+                <div>
+                    <h4>Add A Break</h4>
+                    <div className='details'>
+                        <button>10s</button>
+                    </div>
+                </div>
+                <div>
+                    <h4>Exercise</h4>
+                    <div className='details'>
+                        <h5>Exercise time <span>seconds</span></h5>
+                    </div>
+                    <br></br>
+                    <div className='details'>
+                        <h5>Break time <span>seconds</span></h5>
+                    </div>
+                </div>
+                <button className='activity-btn'>
+                    <p>Activity Completed</p>
+                </button>
         </div>
     );
 };
--- src/components/Items/Items.js
+++ src/components/Items/Items.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import Item from '../Item/Item';
-import logo from '../../images/logo.jpeg';
 import './Items.css'
 import Cart from '../Cart/Cart';
 
@@ -14,16 +13,16 @@ const Items = () => {
         .then(data => setItems(data))
     },[]);
 
-    const handleAddToItem = (item) => {
-        console.log(item);
-        const newTimecount = [...timecount , item];
+    const handleAddToItem = (count) => {
+        console.log(count);
+        const newTimecount = [...timecount , count];
         setTimecount(newTimecount);
     }
 
     return (
         <div className='items-container'>
             <div className='items-body'>
-                <h2 className='exercise'>Select exercise Items: {items.length}</h2>
+                <h2 className='exercise'>Select exercise Items:</h2>
                 <div className='item'>
                 {
                     items.map(item => <Item 
@@ -35,39 +34,7 @@ const Items = () => {
                 </div>
             </div>
             <div className='profile-body'>
-                <Cart></Cart>
-                {/* <h2>Profile: {timecount.length}</h2>
-                <div className='logo'>
-                    <img src={logo} alt=''></img>
-                    <div className='profile-info'>
-                        <h4>Md. Shamim Hossain</h4>
-                        <p>Paltan, Dhaka-1000</p>
-                    </div>
-                </div>
-                <div className='details'>
-                    <p>75kg Weight</p>
-                    <p>5.5 height</p>
-                    <p>28yrs Age</p>
-                </div>
-                <div>
-                    <h4>Add A Break</h4>
-                    <div className='details'>
-                        <button>10s</button>
-                    </div>
-                </div>
-                <div>
-                    <h4>Exercise</h4>
-                    <div className='details'>
-                        <h5>Exercise time <span>seconds</span></h5>
-                    </div>
-                    <br></br>
-                    <div className='details'>
-                        <h5>Break time <span>seconds</span></h5>
-                    </div>
-                </div>
-                <button className='activity-btn'>
-                    <p>Activity Completed</p>
-                </button> */}
+                <Cart cart = {timecount}></Cart>
             </div>
         </div>
     );
